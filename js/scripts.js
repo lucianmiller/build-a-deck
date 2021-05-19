d757cbf6f980caac3ab227885e9f8dcfad70d410
Lucian <lucian.miller185@gmail.com>
2021-05-19 11:40:08 -0700

    It will add arrays for ranks and suits

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
   $("form#button").submit(function(event){
+    const suits = ["of clubs", "of diamonds", "of spades", "of hearts"];
+    console.log(suits);
+    const ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+    console.log(ranks);
     event.preventDefault();
   });
 });
