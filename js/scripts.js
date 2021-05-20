6a8eb54edbf37022935e4363a8be4df1b52ee05e
Lucian <lucian.miller185@gmail.com>
2021-05-20 10:52:16 -0700

    It will loop suits and ranks and combind them together

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,13 @@ $(document).ready(function () {
     console.log(suits);
     const ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
     console.log(ranks);
+
+    suits.forEach(function(suit) {
+      ranks.forEach(function(rank) {
+        $("#deck").append(`<li> ${rank} ${suit} </li>`);
+      });
+    });
+
     event.preventDefault();
   });
 });
