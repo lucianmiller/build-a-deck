ce95ede57031bf91a8915153e7775f5c18e933d6
Lucian <lucian.miller185@gmail.com>
2021-05-20 10:54:27 -0700

    It will delete console logs

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
   $("form#button").submit(function(event){
     const suits = ["of clubs", "of diamonds", "of spades", "of hearts"];
-    console.log(suits);
     const ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
-    console.log(ranks);
 
     suits.forEach(function(suit) {
       ranks.forEach(function(rank) {
